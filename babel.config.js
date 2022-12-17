9e9b2d6e3a295bf5b4fe8183c3e8f9070ba3537b
Natron Kan <waterbeside@163.com>
2022-12-17 21:18:46 +0800

    chore: modules false

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', { modules: false, targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
   plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]]
 }
