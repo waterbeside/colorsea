02d775544d915dfc675523fbfe2a34bb2917b224
Natron Kan <waterbeside@163.com>
2022-12-17 16:23:56 +0800

    feat: 添加用于创建Color实例的creator

---
+++ src/creator.ts
@@ -0,0 +1,20 @@
+import { Color } from './color'
+import { hsl2rgb, hsv2rgb, xyz2rgb, lab2rgb } from './convertor'
+
+export default {
+  rgb(r: number, g: number, b: number, alpha?: number): Color {
+    return new Color([r, g, b], alpha)
+  },
+  hsl(h: number, s: number, l: number, alpha?: number): Color {
+    return new Color(hsl2rgb(h, s, l), alpha)
+  },
+  hsv(h: number, s: number, v: number, alpha?: number): Color {
+    return new Color(hsv2rgb(h, s, v), alpha)
+  },
+  xyz(x: number, y: number, z: number, alpha?: number): Color {
+    return new Color(xyz2rgb(x, y, z), alpha)
+  },
+  lab(l: number, a: number, b: number, alpha?: number): Color {
+    return new Color(lab2rgb(l, a, b), alpha)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Color } from './color'
 import * as convertor from './convertor'
+import creator from './creator'
+
+import type { RgbType, RgbaType } from '..//typings/colorType'
 
 function colorsea(colorSetting: RgbType | RgbaType | string, alpha?: number) {
   return new Color(colorSetting, alpha)
@@ -7,5 +10,10 @@ function colorsea(colorSetting: RgbType | RgbaType | string, alpha?: number) {
 
 colorsea.convertor = convertor
 colorsea.Color = Color
+colorsea.rgb = creator.rgb
+colorsea.hsl = creator.hsl
+colorsea.hsv = creator.hsv
+colorsea.xyz = creator.xyz
+colorsea.lab = creator.lab
 
 export default colorsea
