3d20f385aeb7d82399f20c12705a703a754668b6
Natron Kan <waterbeside@163.com>
2022-12-26 12:02:13 +0800

    fix: 修正rgb hwb转换公式

--- src/convertor/rgbHwb.ts
+++ src/convertor/rgbHwb.ts
@@ -3,11 +3,11 @@ import { CommonColorTuple } from '../../typings/colorType'
 
 export const rgb2hwb = function (r: number, g: number, b: number): CommonColorTuple {
   const [h] = rgb2hsl(r, g, b)
-  const w = (Math.min(r, g, b) / 255) * 100
-  const bb = (1 - Math.max(r, g, b) / 255) * 100
+  const w = Math.min(r, g, b) / 255
+  const bb = 1 - Math.max(r, g, b) / 255
   return [h, w, bb]
 }
 
 export function hwb2rgb(h: number, w: number, b: number): CommonColorTuple {
-  return hsl2rgb(h, 100, 50).map(c => (c * (100 - w - b)) / 100 + h) as CommonColorTuple
+  return hsl2rgb(h, 1, 0.5).map(c => ((c / 255) * (1 - w - b) + w) * 255) as CommonColorTuple
 }
