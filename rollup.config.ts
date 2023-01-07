ef64ad2011d013a62bd9b637e3f590f27d7a953d
Natron Kan <waterbeside@163.com>
2023-01-07 17:20:16 +0800

    chore: 打包color names

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,6 +4,7 @@ import ts from 'rollup-plugin-typescript2'
 import terser from '@rollup/plugin-terser'
 import dts from 'rollup-plugin-dts'
 import fs from 'fs'
+import path from 'path'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }))
 
@@ -32,4 +33,43 @@ const config = [
   })
 ]
 
+// 对指定dir打包的配置
+const rollColorNames = () => {
+  const dirPath = path.join(path.resolve('./src'), 'colors')
+  const outputDir = path.resolve('./colors')
+  const dirNames = fs.readdirSync(dirPath)
+  for (const dirName of dirNames) {
+    const input = path.join(dirPath, dirName)
+    const fileName = /\.(js|ts)$/.test(dirName) ? dirName.slice(0, -3) : dirName
+    const stat = fs.statSync(input)
+    if (!stat.isDirectory()) {
+      config.push(
+        defineConfig({
+          input,
+          output: {
+            name: fileName,
+            file: path.join(outputDir, `${fileName}.js`),
+            format: 'umd'
+          },
+          plugins: [
+            ts({
+              clean: true
+            }),
+            terser()
+          ]
+        }),
+        defineConfig({
+          input,
+          output: {
+            file: path.join(outputDir, `${fileName}.d.ts`),
+            format: 'es'
+          },
+          plugins: [dts()]
+        })
+      )
+    }
+  }
+}
+
+rollColorNames()
 export default config
