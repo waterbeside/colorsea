16abee8921f9cc9f0df8578ddb4e665a8ea14a80
Natron Kan <waterbeside@163.com>
2023-01-07 16:00:53 +0800

    test: 测试color names

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,5 +1,9 @@
 import colorsea from '../src/index'
-describe('Class Color', () => {
+import x11 from '../src/colors/x11'
+
+colorsea.useNames(x11)
+
+describe('test colorsea', () => {
   it('test color operations', () => {
     expect(colorsea('#ffffff').darken(10).hex()).toBe('#e6e6e6')
     expect(colorsea('#000').lighten(10).hex()).toBe('#1a1a1a')
@@ -7,5 +11,14 @@ describe('Class Color', () => {
     expect(colorsea.hsl(80, 90, 20).saturate(10).hsl()).toEqual([80, 100, 20])
     expect(colorsea.hsl(80, 90, 20).desaturate(10).hsl()).toEqual([80, 80, 20])
     expect(colorsea('#776600').fadeOut(10).rgba()).toEqual([119, 102, 0, 90])
+  })
+
+  it('test color names', () => {
+    expect(colorsea('Aqua').hex()).toBe('#00ffff')
+    expect(colorsea('Aquamarine').hex()).toBe('#7fffd4')
+    expect(colorsea('Beige').hex().toUpperCase()).toBe('#F5F5DC')
+    expect(colorsea('DarkCyan').hex().toUpperCase()).toBe('#008B8B')
+    expect(colorsea('DimGray').hex().toUpperCase()).toBe('#696969')
+    expect(colorsea('LightCoral').hex().toUpperCase()).toBe('#F08080')
   })
 })
