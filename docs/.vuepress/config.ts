dd90ad8e814f72fc1569e76c832387088284a2ce
Natron Kan <waterbeside@163.com>
2023-01-09 15:45:40 +0800

    chore: upgrade vuepress1 to vuepress2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,9 +1,28 @@
-import { defineConfig, PluginTuple } from 'vuepress/config'
+import { defineUserConfig, defaultTheme } from 'vuepress'
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+import fullTextSearchPlugin from "vuepress-plugin-full-text-search2"
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
 
-export default defineConfig({
-  evergreen: true,
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineUserConfig({
+  // evergreen: true,
   base: '/colorsea/',
-  plugins: ['fulltext-search' as PluginTuple, '@vuepress/plugin-back-to-top'],
+  plugins: [
+    fullTextSearchPlugin,
+    backToTopPlugin(),
+    registerComponentsPlugin({
+      components: {
+        ColorBox: path.resolve(__dirname, './components/ColorBox.vue'),
+        ColorNameItem: path.resolve(__dirname, './components/ColorNameItem.vue'),
+        X11List: path.resolve(__dirname, './components/X11List.vue'),
+        ChineseColorList: path.resolve(__dirname, './components/ChineseColorList.vue'),
+        NipponColorList: path.resolve(__dirname, './components/NipponColorList.vue'),
+      },
+    }),
+  ],
   locales: {
     '/': {
       lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
@@ -16,62 +35,28 @@ export default defineConfig({
       description: '简单的颜色空间转换和颜色操作库'
     }
   },
-  themeConfig: {
+  theme: defaultTheme({
     repo: 'waterbeside/colorsea',
-    smoothScroll: true,
+    // smoothScroll: true,
     locales: {
       '/': {
-        selectText: 'Languages',
-        label: 'English',
-        ariaLabel: 'Languages',
+        // selectText: 'Languages',
+        selectLanguageName: 'English',
+        // label: 'English',
+        // ariaLabel: 'Languages',
         // 显示所有（非活动页面）标题组成的链接
         // displayAllHeaders: false, // 默认值：false
         // 滚动操作是否改变标题链接的显示
-        activeHeaderLinks: true, // 默认值：true
-        sidebar: [
-          {
-            title: 'About',
-            path: '/about/'
-          },
-          {
-            title: 'Quick start',
-            path: '/quickStart/'
-          },
-          {
-            title: 'Color operations',
-            path: '/operate/'
-          },
-          {
-            title: 'Color space',
-            path: '/space/'
-          }
-        ]
+        // activeHeaderLinks: true // 默认值：true
       },
       '/zh/': {
-        // 多语言下拉菜单的标题
-        selectText: 'Languages',
-        // 该语言在下拉菜单中的标签
-        label: '简体中文',
-        sidebar: [
-          {
-            title: '介绍',
-            path: '/about/'
-          },
-          {
-            title: '快速上手',
-            path: '/quickStart/'
-          },
-          {
-            title: '颜色操作',
-            path: '/operate/'
-          },
-          {
-            title: '颜色空间',
-            path: '/space/'
-          }
-        ]
+        selectLanguageName: '简体中文',
+        // // 多语言下拉菜单的标题
+        // selectText: 'Languages',
+        // // 该语言在下拉菜单中的标签
+        // label: '简体中文'
       },
 
     }
-  }
+  })
 })
