51e58f6e3f068056b3d973cfb285868e57749949
Natron Kan <waterbeside@163.com>
2023-03-10 15:34:21 +0800

    fix: Fixed calculation of color.mix

--- src/utils/mix.ts
+++ src/utils/mix.ts
@@ -2,6 +2,9 @@ import { Color } from '../color'
 import { clamp } from '../utils/'
 import type { CommonColorTuple, CommonColoraTuple } from '../../typings/colorType'
 
+/**
+ * reference: https://github.com/sass/dart-sass/
+ */
 export const mix = function (
   color1: Color | string | CommonColoraTuple | CommonColorTuple,
   color2: Color | string | CommonColoraTuple | CommonColorTuple,
@@ -9,8 +12,8 @@ export const mix = function (
 ) {
   const c1 = color1 instanceof Color ? color1 : new Color(color1)
   const c2 = color2 instanceof Color ? color2 : new Color(color2)
-  weight /= 100
-  const w = clamp(weight, 0, 1) * 2 - 1
+  const wScale = clamp(weight, 0, 100) / 100
+  const w = wScale * 2 - 1
   const d = c1.alpha() - c2.alpha()
 
   const combinedWeight1 = w * d == -1 ? w : (w + d) / (1 + w * d)
@@ -22,5 +25,6 @@ export const mix = function (
     Math.round(clamp(c1.green() * w1 + c2.green() * w2, 0, 255)),
     Math.round(clamp(c1.blue() * w1 + c2.blue() * w2, 0, 255))
   ]
-  return new Color(rgb, c1.alpha() * w + c2.alpha() * (1 - w))
+  const alpha = c1.alpha() * wScale + c2.alpha() * (1 - wScale)
+  return new Color(rgb, alpha)
 }
