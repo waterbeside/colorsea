603b8919158597da3d2c14c02a877d888b32f47e
Natron Kan <waterbeside@163.com>
2023-03-10 17:17:19 +0800

    docs: 使ColorBox的文字颜色自动根据背景色调整

--- docs/.vuepress/components/ColorBox.vue
+++ docs/.vuepress/components/ColorBox.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import { defineProps, computed } from 'vue'
+import colorsea from '../../../src/index'
 
 const props = defineProps({
   boxColor: {
@@ -8,27 +9,71 @@ const props = defineProps({
   },
   textColor: {
     type: String,
-    default: '#fff'
+    require: false
   },
   minWidth: {
+    type: [Number, String],
+    default: '1em'
+  },
+  alpha: {
     type: Number,
-    default: 100
+    require: false    
+  },
+  showBorder: {
+    type: Boolean,
+    default: true,
   }
 })
+
+const miniWidth = computed(() => {
+  if (typeof props.minWidth === 'number') return props.minWidth + 'px'
+  return props.minWidth
+})
+
+const bgColor = computed(() => {
+  if (props.alpha !== void 0) return colorsea(props.boxColor, props.alpha)
+  return colorsea(props.boxColor)
+})
+
+const fontColor = computed(() => {
+  if (props.textColor) return props.textColor
+  if (bgColor.value.brightness() > 50) return bgColor.value.alpha(100).mix('#000000', 30).darken(30).hex()
+  return bgColor.value.alpha(100).mix('#ffffff', 40).lighten(30).hex()
+})
+
+const borderColor = computed(() => {
+  return bgColor.value.mix('#000000', 10).darken()
+})
+
 
 </script>
 
 <template>
-  <div class="colorbox" :style="{backgroundColor: boxColor, color: textColor, minWidth: minWidth + 'px'}">
+  <div
+    class="colorbox"
+    :class="{'colorbox--border': showBorder}"
+    :style="{
+      backgroundColor: bgColor.hex(),
+      color: fontColor,
+      minWidth: miniWidth,
+      '--border-color': borderColor.hex()
+    }"
+  >
     <slot></slot>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .colorbox {
-  display: inline-block;
-  min-height: 20px;
+  display: inline-flex;
+  box-sizing: content-box;
+  min-height: 1.2em;
   border-radius: 5px;
-  padding: 2px 12px;
+  justify-content: center;
+  align-items: center;
+  padding: 0.2em 0.5em;
+  &--border {
+    border: 1px solid var(--border-color)
+  }
 }
 </style>
